4f117c6e609162c38da4f69ef52053ea67bb79ef
Bohao Chen <bohaoc@curacloudcorp.com>
2016-11-20 19:58:26 +0800

    [Animation]chapter5 done

--- Animation/FlightInfo/Flight Info/ViewController.swift
+++ Animation/FlightInfo/Flight Info/ViewController.swift
@@ -78,6 +78,8 @@ class ViewController: UIViewController {
         // populate the UI with the next flight's data
         summary.text = data.summary
         if animated {
+            planeDepart()
+            
             fade(imageView: bgImageView, toImage: UIImage(named: data.weatherImageName)!, showEffects: data.showWeatherEffects)
             let direction: AnimationDirection = data.isTakingOff ? .positive : .negative
             
@@ -164,8 +166,33 @@ class ViewController: UIViewController {
             label.alpha = 1.0
             label.transform = .identity
         })
+    }
+    
+    func planeDepart() {
+        let originalCenter = planeImage.center
         
-        
+        UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25, animations: {
+                self.planeImage.center.x += 80.0
+                self.planeImage.center.y -= 10.0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4, animations: { 
+                self.planeImage.transform = CGAffineTransform(rotationAngle: -.pi / 8)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: { 
+                self.planeImage.center.x += 100.0
+                self.planeImage.center.y -= 50.0
+                self.planeImage.alpha = 0.0
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01, animations: { 
+                self.planeImage.transform = .identity
+                self.planeImage.center = CGPoint(x: 0.0, y: originalCenter.y)
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45, animations: { 
+                self.planeImage.alpha = 1.0
+                self.planeImage.center = originalCenter
+            })
+        }, completion: nil)
     }
   
   
