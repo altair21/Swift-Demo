28dd87f34765e3afe70719ca3151d286f1cf158a
Bohao Chen <bohaoc@curacloudcorp.com>
2016-11-23 15:38:17 +0800

    [Animation]chapter12 done

--- Animation/BahamaAir-Layer/BahamaAirLoginScreen/ViewController.swift
+++ Animation/BahamaAir-Layer/BahamaAirLoginScreen/ViewController.swift
@@ -253,6 +253,13 @@ class ViewController: UIViewController {
         tintBackgroundColor(layer: self.loginButton.layer, toColor: tintColor)
         roundCorners(layer: self.loginButton.layer, toRadius: 10.0)
     })
+    
+    let wobble = `CAKeyframeAnimation`(keyPath: "transform.rotation")
+    wobble.duration = 0.25
+    wobble.repeatCount = 4
+    wobble.values = [0.0, -.pi/4.0, 0.0, .pi/4.0, 0.0]
+    wobble.keyTimes = [0.0, 0.25, 0.5, 0.75, 1.0]
+    heading.layer.add(wobble, forKey: nil)
   }
 
   // MARK: further methods
@@ -283,6 +290,22 @@ class ViewController: UIViewController {
     let tintColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
     tintBackgroundColor(layer: loginButton.layer, toColor: tintColor)
     roundCorners(layer: loginButton.layer, toRadius: 25.0)
+    
+    let balloon = CALayer()
+    balloon.contents = UIImage(named: "balloon")!.cgImage
+    balloon.frame = CGRect(x: -50.0, y: 0.0, width: 50.0, height: 65.0)
+    view.layer.insertSublayer(balloon, below: username.layer)
+    
+    let flight = CAKeyframeAnimation(keyPath: "position")
+    flight.duration = 12.0
+    flight.values = [
+        CGPoint(x: -50.0, y: 0.0),
+        CGPoint(x: view.frame.width + 50.0, y: 160.0),
+        CGPoint(x: -50.0, y: loginButton.center.y)
+        ].map { NSValue(cgPoint: $0) }
+    flight.keyTimes = [0.0, 0.5, 1.0]
+    balloon.add(flight, forKey: nil)
+    balloon.position = CGPoint(x: -50.0, y: loginButton.center.y)
   }
 
   func animateCloud(_ layer: CALayer) {
